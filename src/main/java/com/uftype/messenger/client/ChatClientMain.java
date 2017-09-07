8603492cf10c51573e51ad4af6c99e17f1d55b77
Annie <annhiluc@gmail.com>
2017-09-07 15:18:50 -0400

    Added Protocol Buffer for message protocol

--- src/main/java/com/uftype/messenger/client/ChatClientMain.java
+++ src/main/java/com/uftype/messenger/client/ChatClientMain.java
@@ -1,11 +1,19 @@
 package com.uftype.messenger.client;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class ChatClientMain {
     public static void main(String[] args) throws IOException {
         try {
-            ChatClient client = new ChatClient("127.0.0.1", 3000);
+            Scanner in = new Scanner(System.in);
+
+            System.out.println("Please enter the host you want to chat with: ");
+            String host = in.nextLine();
+            System.out.println("Please enter the port you want to communicate on: ");
+            int port = in.nextInt();
+
+            ChatClient client = new ChatClient(host, port);
         } catch (IOException e) {
         }
     }
--- src/main/java/com/uftype/messenger/common/ChatConnection.java
+++ src/main/java/com/uftype/messenger/common/ChatConnection.java
@@ -1,13 +1,16 @@
 package com.uftype.messenger.common;
 
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.uftype.messenger.proto.ChatMessage;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class ChatConnection {
     public static Socket socket; // Socket used for communication
     public static BufferedReader screenReader, receiveReader; // Reader for reading the text on the screen, other for reading the text from connection
-    public static PrintWriter writer; // Writer to the connection
     public static OutputStream outputStream; // Output stream of the socket for writing to connection
     public static InputStream inputStream; // Input stream of the socket for reading from connection
 
@@ -41,8 +44,6 @@ public class ChatConnection {
         try {
             screenReader = new BufferedReader(new InputStreamReader(System.in));
             outputStream = socket.getOutputStream();
-            writer = new PrintWriter(outputStream, true);
-
             inputStream = socket.getInputStream();
             receiveReader = new BufferedReader(new InputStreamReader(inputStream));
         } catch (IOException e) {
@@ -54,16 +55,23 @@ public class ChatConnection {
         public void run() {
             try {
                 String sendMsg;
+                ChatMessage.Message.Builder message = ChatMessage.Message.newBuilder();
+
                 while (isReceiving) {
                     // Create two threads, one to listen for messages, one for listening to typing on screen
                     sendMsg = screenReader.readLine();
-                    writer.println(sendMsg);
-                    writer.flush();
+
+                    // Build message here
+                    message.setText(sendMsg);
+                    message.build().writeDelimitedTo(outputStream);
                 }
-            } catch (IOException e) {
-                System.out.println("Error while sending message from UF TYPE messenger: " + e);
-            } catch (NullPointerException e) {
-                System.out.println("Connection has not been properly initialized: " + e);
+            } catch (Exception e) {
+                if (e.getMessage().equals("Connection reset")) {
+                    System.out.println("Connection closed with the remote host.");
+                }
+                else {
+                    System.out.println("Error while receiving message from UF TYPE messenger: " + e);
+                }
             }
         }
     }
@@ -74,14 +82,16 @@ public class ChatConnection {
                 String receiveMsg;
                 while (isReceiving) {
                     // Create two threads, one to listen for messages, one for listening to typing on screen
-                    if ((receiveMsg = receiveReader.readLine()) != null) {
-                        System.out.println(receiveMsg);
-                    }
+                    ChatMessage.Message message = ChatMessage.Message.parseDelimitedFrom(inputStream);
+                    System.out.println(message.getText());
                 }
-            } catch (IOException e) {
-                System.out.println("Error while receiving message from UF TYPE messenger: " + e);
-            } catch (NullPointerException e) {
-                System.out.println("Connection has not been properly initialized: " + e);
+            } catch (Exception e) {
+                if (e.getMessage().equals("Connection reset")) {
+                    System.out.println("Connection closed with the remote host.");
+                }
+                else {
+                    System.out.println("Error while receiving message from UF TYPE messenger: " + e);
+                }
             }
         }
     }
