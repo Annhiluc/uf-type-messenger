c4981568ecb74df38776c6f7c10a904802c4a595
Annie <annhiluc@gmail.com>
2017-09-10 16:25:28 -0400

    Adding authentication using MD5 hash and salt

--- src/main/java/com/uftype/messenger/auth/Authentication.java
+++ src/main/java/com/uftype/messenger/auth/Authentication.java
@@ -1,27 +1,70 @@
 package com.uftype.messenger.auth;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Authentication {
-    public void login() {
+    private final static Logger LOGGER = Logger.getLogger(Authentication.class.getName()); // Logger to provide information to server
+    private final static Random RANDOM = new Random();
 
+    public static boolean login(String username, String password) {
+        return authenticate(username, password);
     }
 
-    public void logout() {
-
+    public static boolean logout(String username) {
+        return false;
     }
 
-    public void register() {
+    public static boolean register(String firstname, String lastname, String username, String email, String password) {
+        boolean isRegistered = false;
+        Database.connect();
 
+        String salt = generateSalt();
+        String securePassword = hashMD5(password, salt);
+
+        isRegistered = Database.register(firstname, lastname, username, email, securePassword, salt);
+
+        Database.disconnect();
+        return isRegistered;
+    }
+
+    public static boolean authenticate(String username, String password) {
+        boolean isAuthenticated = false;
+        Database.connect();
+
+        isAuthenticated = Database.validate(username, password);
+
+        Database.disconnect();
+        return isAuthenticated;
+    }
+
+    public static String hashMD5(String password, String salt) {
+        String hashed = null;
+
+        try {
+            MessageDigest digest = MessageDigest.getInstance("MD5");
+            String toDigest = password + salt;
+
+            digest.update(toDigest.getBytes());
+            hashed = new BigInteger(1, digest.digest()).toString(16);
+        } catch (NoSuchAlgorithmException e) {
+            LOGGER.log(Level.WARNING, "Unable to hash the password: " + e);
+        }
+
+        return hashed;
+    }
+
+    public static String generateSalt() {
+        byte[] salt = new byte[16];
+        RANDOM.nextBytes(salt);
+        return salt.toString();
     }
 
     public void resetPassword() {
-
-    }
-
-    public void authenticate() {
-
-    }
-
-    public void encrypt() {
 
     }
 }
--- src/main/java/com/uftype/messenger/auth/Database.java
+++ src/main/java/com/uftype/messenger/auth/Database.java
@@ -4,11 +4,25 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.*;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Database {
-    public void connect() throws SQLException{
+    private static Connection conn;
+    private final static Logger LOGGER = Logger.getLogger(Database.class.getName()); // Logger to provide information to server
+
+    public synchronized static void connect() {
+        if (conn != null) {
+            // Connection is already established
+            return;
+        }
+
         // Load the Oracle JDBC driver
-        DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+        try {
+            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+        } catch (SQLException e) {
+            LOGGER.log(Level.WARNING, "Unable to register the JDBC driver: " + e);
+        }
 
         Properties properties = new Properties();
         try {
@@ -21,32 +35,80 @@ public class Database {
         // You must put a database name after the @ sign in the connection URL.
         // You can use either the fully specified SQL*net syntax or a short cut
         // syntax as <host>:<port>:<sid>.  The example uses the short cut syntax.
-        Connection conn = DriverManager.getConnection (properties.getProperty("url"),
-                properties.getProperty("username"), properties.getProperty("password"));
-
-        // Create a Statement
-        Statement stmt = conn.createStatement ();
-
-        // Select the ENAME column from the EMP table
-        ResultSet rset = stmt.executeQuery ("select * from COURSE");
-
-        // Iterate through the result and print the employee names
-        while (rset.next())
-            System.out.println (rset.getString (2));
-
-        conn.close(); // ** IMPORTANT : Close connections when done **
+        try {
+            conn = DriverManager.getConnection (properties.getProperty("url"),
+                    properties.getProperty("username"), properties.getProperty("password"));
+        } catch (SQLException e) {
+            LOGGER.log(Level.WARNING, "Unable to connect to the database: " + e);
+        }
     }
 
-    public void getUser() {
-
+    public synchronized static void disconnect() {
+        try {
+            if (conn != null) {
+                conn.close(); // ** IMPORTANT : Close connections when done **
+            }
+        } catch (SQLException e) {
+            LOGGER.log(Level.WARNING, "Unable to close the database connection: " + e);
+        }
     }
 
-    public void getPassword() {
+    public synchronized static boolean register(String firstname, String lastname, String username, String email, String securePassword, String salt) {
+        try {
+            // Create a Statement
+            Statement stmt = conn.createStatement ();
 
+            // Select the ENAME column from the EMP table
+            ResultSet usernameCheck = stmt.executeQuery ("select * from user_account where user_name = '" + username + "'");
+            ResultSet emailCheck = stmt.executeQuery ("select * from user_account where email = '" + email + "'");
+
+            if (usernameCheck.next() || emailCheck.next()) {
+                return false; // Email or username already exists
+            }
+
+            String query = "insert into user_account " +
+                    "(first_name, last_name, user_name, email, password, password_salt) values ('" +
+                    firstname + "', '" + lastname + "', '" + username + "', '" + email + "', '" + securePassword + "', '" + salt + "')";
+
+            ResultSet insertUser = stmt.executeQuery(query);
+            ResultSet commitUser = stmt.executeQuery("commit");
+        } catch (SQLException e) {
+            LOGGER.log(Level.WARNING, "Unable to register user: " + e);
+        }
+
+        return true;
+    }
+
+    public synchronized static boolean validate(String username, String password) {
+        try {
+            // Create a Statement
+            Statement stmt = conn.createStatement ();
+
+            // Select the ENAME column from the EMP table
+            ResultSet rset = stmt.executeQuery ("select password, password_salt from user_account where user_name = '" + username + "'");
+
+            if (rset.next()) {
+                String salt = rset.getString("password_salt");
+                String passwordToCompare = rset.getString("password");
+                return Authentication.hashMD5(password, salt).equals(passwordToCompare);
+            }
+            else {
+                return false;
+            }
+
+        } catch (SQLException e) {
+            LOGGER.log(Level.WARNING, "Unable to retrieve validate user information: " + e);
+        }
+
+        return false;
     }
 
     public static void main(String[] args) throws SQLException{
-        Database database = new Database();
-        database.connect();
+        Database.connect();
+
+        Database.register("Test", "Name", "testuser", "test@gmail.com", Authentication.hashMD5("password", "salt"), "salt");
+        System.out.println(Database.validate("testuser", "password"));
+
+        Database.disconnect();
     }
 }
