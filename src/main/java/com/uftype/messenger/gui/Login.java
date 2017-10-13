35aa4788e8a54f1b4fb40a3fbd2cb24c1e1e87cd
Annie <annhiluc@gmail.com>
2017-10-12 23:14:19 -0400

    Added registration to login page

--- src/main/java/com/uftype/messenger/gui/Login.java
+++ src/main/java/com/uftype/messenger/gui/Login.java
@@ -11,26 +11,31 @@ import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
 public class Login extends JFrame implements ActionListener, WindowListener {
-    protected JTextField username;
+    protected JTextField username, firstName, lastName, email;
     protected JPasswordField password;
-    protected JPanel title, loginPanel, submitPanel;
-    protected JButton submit;
+    protected JPanel title, loginPanel, submitPanel, registerPanel;
+    protected JButton submit, loginRegister;
     public UserContext loggedInUser;
     protected GUI gui;
 
     public Login (GUI gui) {
-        super("Login");
+        super("UF TYPE Messenger");
 
         loggedInUser = null;
         this.gui = gui;
 
-        title = new JPanel(new GridLayout(2,1));
+        loginRegister = new JButton("Register");
+        loginRegister.addActionListener(this);
+        loginRegister.setEnabled(true);
+
+        title = new JPanel(new GridLayout(3,1));
         ImageIcon ii = new ImageIcon("src/main/resources/type-logo.png");
         JLabel label = new JLabel(ii);
         JLabel welcome = new JLabel("Welcome to the UF TYPE Messenger!");
         welcome.setHorizontalAlignment(0);
         title.add(label);
         title.add(welcome);
+        title.add(loginRegister);
         add(title, BorderLayout.NORTH);
 
         loginPanel = new JPanel(new GridLayout(2,1));
@@ -44,6 +49,33 @@ public class Login extends JFrame implements ActionListener, WindowListener {
         loginPanel.add(passLabel);
         loginPanel.add(password);
         add(loginPanel, BorderLayout.CENTER);
+
+        registerPanel = new JPanel(new GridLayout(5, 1));
+        JLabel firstLabel = new JLabel("First name:");
+        firstName = new JTextField(1);
+        JLabel lastLabel = new JLabel("Last name:");
+        lastName = new JTextField(1);
+        userLabel = new JLabel("Username:");
+        username = new JTextField(1);
+        passLabel = new JLabel("Password:");
+        password = new JPasswordField(1);
+        // Consider adding another password field here to verify password
+        JLabel emailLabel = new JLabel("Email:");
+        email = new JTextField(1);
+        // Consider adding another email field here to verify email
+
+        registerPanel.add(firstLabel);
+        registerPanel.add(firstName);
+        registerPanel.add(lastLabel);
+        registerPanel.add(lastName);
+        registerPanel.add(userLabel);
+        registerPanel.add(username);
+        registerPanel.add(passLabel);
+        registerPanel.add(password);
+        registerPanel.add(emailLabel);
+        registerPanel.add(email);
+
+        // Will add this panel to frame when register button pressed
 
         submitPanel = new JPanel(new GridLayout(1,1));
         submit = new JButton("Submit");
@@ -78,21 +110,60 @@ public class Login extends JFrame implements ActionListener, WindowListener {
         Object o = e.getSource();
 
         if (o == submit) {
-            // Handle other actions
-            String user = username.getText().trim();
-            String pass = new String(password.getPassword()).trim();
+            if (loginRegister.getText().equals("Register")) {
+                // On login page
+                String user = username.getText().trim();
+                String pass = new String(password.getPassword()).trim();
 
-            if (Authentication.authenticate(user, pass) != null) {
-                loggedInUser = Authentication.login(user, pass);
-                gui.loadScreen();
-                // This window closes.
-                dispose();
+                if (Authentication.authenticate(user, pass) != null) {
+                    loggedInUser = Authentication.login(user, pass);
+                    gui.loadScreen();
+                    // This window closes.
+                    dispose();
+                }
+                else {
+                    JOptionPane.showMessageDialog(Login.this, "Incorrect credentials. Please try again.");
+                    username.setText("");
+                    password.setText("");
+                    return;
+                }
             }
             else {
-                JOptionPane.showMessageDialog(Login.this, "Incorrect credentials. Please try again.");
-                username.setText("");
-                password.setText("");
-                return;
+                // On register page
+                String user = username.getText().trim();
+                String pass = new String(password.getPassword()).trim();
+                String em = email.getText().trim();
+                String first = firstName.getText().trim();
+                String last = lastName.getText().trim();
+
+                if (Authentication.register(first, last, user, em, pass)) {
+                    loggedInUser = Authentication.login(user, pass);
+                    gui.loadScreen();
+                    // This window closes.
+                    dispose();
+                }
+                else {
+                    JOptionPane.showMessageDialog(Login.this, "Invalid credentials. Please try again.");
+                    username.setText("");
+                    password.setText("");
+                    email.setText("");
+                    firstName.setText("");
+                    lastName.setText("");
+                    return;
+                }
+            }
+        }
+        else if (o == loginRegister) {
+            if (loginRegister.getText().equals("Register")) {
+                // Switch to register page with fields for name, username, etc.
+                remove(loginPanel);
+                add(registerPanel, BorderLayout.CENTER);
+                loginRegister.setText("Login");
+            } else {
+                // Switch to login page
+                remove(registerPanel);
+                add(loginPanel, BorderLayout.CENTER);
+                loginRegister.setText("Register");
             }
         }
     }
